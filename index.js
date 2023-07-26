2556c5349c95a2dbd7587e92dbc37000e3cf0d19
SABA SAHAR <125450313+SABASAHAR123@users.noreply.github.com>
2023-07-26 09:56:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+// const mongoose = require('mongoose')
+require('dotenv').config()
+const port = process.env.SERVER_PORT
+const cors = require('cors')
+
+
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api', require('./api/users/router'))
+app.use('/api', require('./api/products/router'))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => console.log("DB Connected"))
+//     .catch((err) => console.log(err.message))
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
